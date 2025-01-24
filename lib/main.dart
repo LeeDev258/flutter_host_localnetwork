a74c27b75cc7651835777e15b376aea31f31205f
lee <dechawit@smilepos.com>
2025-01-24 12:30:53 +0700

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -40,6 +41,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String currentStatus = 'Not started';
+
+  HttpServer? _server;
+  get server => _server;
+  set server(value) => _server = value;
+
+  StreamController<String> stcDetails = StreamController();
+  StreamController<String> stcCurrentStatus = StreamController();
+
   Future<void> downloadFile(String fileId) async {
     final url = 'https://drive.google.com/uc?export=download&id=$fileId';
     final response = await http.get(Uri.parse(url));
@@ -55,15 +65,17 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _incrementCounter() async {
+    if (!Platform.isMacOS) {
+      if (await Permission.manageExternalStorage.request().isGranted) {
+        print("Permission granted");
+      } else {
+        print("Permission denied");
+      }
+    }
     // downloadFile('1nojFYmlAhTYHbeY2UJpQSHYU0PRLnZwi');
     await downloadAndUnzip('1nojFYmlAhTYHbeY2UJpQSHYU0PRLnZwi');
-    // if (await Permission.manageExternalStorage.request().isGranted) {
-    //   print("Permission granted");
-    // } else {
-    //   print("Permission denied");
-    // }
     // await createDirectory();
-    // await runLacalServer();
+    await runLacalServer();
     // var server = await HttpServer.bind(
     //   InternetAddress.anyIPv4, // เปิดให้เข้าถึงจากเครือข่ายเดียวกัน
     //   8080, // พอร์ตสำหรับเซิร์ฟเวอร์
@@ -87,15 +99,28 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> runLacalServer() async {
+    stcCurrentStatus.add('RunServer');
     // โฟลเดอร์ที่เก็บไฟล์ Flutter Web
+    final tempDir = await getApplicationDocumentsDirectory(); // โฟลเดอร์ชั่วคราว
+    final zipFilePath = '${tempDir.path}/downloaded_file.zip';
+    final outputDirPath = '${tempDir.path}/unzipped_files';
     final directory = await getDownloadsDirectory();
-    final webDir = directory; // เส้นทางไปยังโฟลเดอร์ build/web
+    final webDir = '${tempDir.path}/unzipped_files/web'; // เส้นทางไปยังโฟลเดอร์ build/web
 
     // ใช้ shelf_static ให้บริการไฟล์จากโฟลเดอร์
-    var handler = createStaticHandler('${webDir!.path}/web', defaultDocument: 'index.html');
+    var handler = createStaticHandler(webDir, defaultDocument: 'index.html');
 
     // รันเซิร์ฟเวอร์เพื่อให้บริการไฟล์
-    await shelf_io.serve(handler, '0.0.0.0', 2324);
+    await server?.close();
+    server = await shelf_io.serve(handler, '0.0.0.0', 8080);
+    stcDetails.add('Server running at http://${server.address.address}:${server.port}/');
+  }
+
+  Future<void> stopServer() async {
+    stcCurrentStatus.add('StopServer');
+    stcDetails.add('StopServer start');
+    await server?.close();
+    stcDetails.add('StopServer complete');
   }
 
   @override
@@ -109,66 +134,113 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '5555 test local network',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            StreamBuilder<String>(
+                stream: stcCurrentStatus.stream,
+                builder: (context, snapshot) {
+                  return Text(
+                    snapshot.data ?? currentStatus,
+                  );
+                }),
+            StreamBuilder<String>(
+                stream: stcDetails.stream,
+                builder: (context, snapshot) {
+                  return Visibility(
+                    visible: snapshot.hasData,
+                    child: Text(
+                      snapshot.data ?? '',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  );
+                }),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: Container(
+              width: 200,
+              height: 50,
+              child: Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+                Icon(Icons.satellite_alt_rounded),
+                SizedBox(width: 10),
+                Text('Start Server'),
+              ]),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: stopServer,
+            child: Container(
+              width: 200,
+              height: 50,
+              child: Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+                Icon(Icons.stop_rounded),
+                SizedBox(width: 10),
+                Text('Stop Server'),
+              ]),
+            ),
+          ),
+        ],
       ),
     );
   }
-}
 
-Future<void> downloadAndUnzip(String fileId) async {
-  // final downloadUrl = 'https://drive.google.com/uc?export=download&id=$fileId';
-  final downloadUrl = 'https://www.dropbox.com/scl/fi/tpx6vem3iojmnlx1o74zs/test_host_localnetwork.zip?rlkey=k7kj7mzmtqmox0twg3wq1p1d9&st=lnsqyj35&dl=1';
-  final tempDir = await getDownloadsDirectory(); // โฟลเดอร์ชั่วคราว
-  final zipFilePath = '${tempDir!.path}/downloaded_file.zip';
-  final outputDirPath = '${tempDir.path}/unzipped_files';
+  Future<void> downloadAndUnzip(String fileId) async {
+    stcCurrentStatus.add('DownloadAndUnzip');
+    final downloadUrl = 'https://drive.google.com/uc?export=download&id=$fileId';
+    // final downloadUrl = 'https://www.dropbox.com/scl/fi/tpx6vem3iojmnlx1o74zs/test_host_localnetwork.zip?rlkey=k7kj7mzmtqmox0twg3wq1p1d9&st=lnsqyj35&dl=1';
+    final tempDir = await getApplicationDocumentsDirectory(); // โฟลเดอร์ชั่วคราว
+    final zipFilePath = '${tempDir.path}/downloaded_file.zip';
+    final outputDirPath = '${tempDir.path}/unzipped_files';
 
-  try {
-    // 1. ดาวน์โหลดไฟล์ .zip
-    print('Downloading file...');
-    var response = await Dio().download(downloadUrl, zipFilePath, onReceiveProgress: (received, total) {
-      if (total != -1) {
-        print('Progress: ${(received / total * 100).toStringAsFixed(0)}%');
+    try {
+      // 1. ดาวน์โหลดไฟล์ .zip
+      print('Downloading file...');
+      stcDetails.add('Downloading file...');
+      var response = await Dio().download(downloadUrl, zipFilePath, onReceiveProgress: (received, total) {
+        if (total != -1) {
+          var percentage = 'Progress: ${(received / total * 100).toStringAsFixed(0)}%';
+          print(percentage);
+          stcDetails.add(percentage);
+        }
+      });
+      print('Download complete: $zipFilePath');
+      stcDetails.add('Download complete: $zipFilePath');
+
+      // 2. แตกไฟล์ .zip
+      print('Unzipping file...');
+      stcDetails.add('Unzipping file...');
+      final zipFile = File(zipFilePath);
+      final bytes = zipFile.readAsBytesSync();
+      final archive = ZipDecoder().decodeBytes(bytes);
+      for (final file in archive) {
+        final filePath = '$outputDirPath/${file.name}';
+        if (file.isFile) {
+          final output = File(filePath);
+          await output.create(recursive: true);
+          await output.writeAsBytes(file.content as List<int>);
+        } else {
+          await Directory(filePath).create(recursive: true);
+        }
       }
-    });
-    print('Download complete: $zipFilePath');
-
-    // 2. แตกไฟล์ .zip
-    print('Unzipping file...');
-    final zipFile = File(zipFilePath);
-    final bytes = zipFile.readAsBytesSync();
-    final archive = ZipDecoder().decodeBytes(bytes);
-    for (final file in archive) {
-      final filePath = '$outputDirPath/${file.name}';
-      if (file.isFile) {
-        final output = File(filePath);
-        await output.create(recursive: true);
-        await output.writeAsBytes(file.content as List<int>);
-      } else {
-        await Directory(filePath).create(recursive: true);
+      print('Unzip complete. Files extracted to: $outputDirPath');
+      stcDetails.add('Unzip complete. Files extracted to: $outputDirPath');
+    } catch (e) {
+      print('Error: $e');
+    } finally {
+      // คุณสามารถลบไฟล์ zip หลังเสร็จงานถ้าต้องการ
+      final zipFile = File(zipFilePath);
+      if (await zipFile.exists()) {
+        await zipFile.delete();
+        print('Temporary zip file deleted.');
+        stcDetails.add('Temporary zip file deleted.');
       }
-    }
-    print('Unzip complete. Files extracted to: $outputDirPath');
-  } catch (e) {
-    print('Error: $e');
-  } finally {
-    // คุณสามารถลบไฟล์ zip หลังเสร็จงานถ้าต้องการ
-    final zipFile = File(zipFilePath);
-    if (await zipFile.exists()) {
-      await zipFile.delete();
-      print('Temporary zip file deleted.');
     }
   }
 }
